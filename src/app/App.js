0e535d0eda7564d17687d9b81464e80211630f98
Vicente Matias <vicente.matias.g@gmail.com>
2023-02-28 18:04:50 -0300

    first commit - project react

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+//App viaja para componente index.js y retorna todo lo que esta en return(h1)
+class App extends Component {
+    render () {
+        return(
+            <h1>Holis</h1> 
+        )
+    }
+}
+
+export default App;
